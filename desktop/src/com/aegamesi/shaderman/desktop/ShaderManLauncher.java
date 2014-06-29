277f03fbd5746d512e67504bfd7199f826f9e8ba
PickleMan <admin.aegamesi@gmail.com>
2014-06-29 13:26:00 -0500

    Setup Swing

---
+++ desktop/src/com/aegamesi/shaderman/desktop/ShaderManLauncher.java
@@ -0,0 +1,27 @@
+package com.aegamesi.shaderman.desktop;
+
+import javax.swing.JFrame;
+
+import com.aegamesi.shaderman.ShaderMan;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
+
+public class ShaderManLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		ShaderMan shaderMan = new ShaderMan();
+		LwjglCanvas canvas = new LwjglCanvas(shaderMan, config);
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				Runtime.getRuntime().halt(0); // address "deadlock causing Swing shutdown hooks"
+			}
+		});
+
+		JFrame frame = new JFrame("ShaderMan Studio");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new ShaderManUI(frame, canvas, shaderMan));
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
---
+++ desktop/src/com/aegamesi/shaderman/desktop/ShaderManUI.java
@@ -0,0 +1,50 @@
+package com.aegamesi.shaderman.desktop;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.aegamesi.shaderman.ShaderMan;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
+
+public class ShaderManUI extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	public JFrame frame;
+	public LwjglCanvas canvas;
+	public ShaderMan shaderMan;
+
+	public ShaderManUI(JFrame frame, LwjglCanvas canvas, ShaderMan shaderMan) {
+		this.frame = frame;
+		this.canvas = canvas;
+		this.shaderMan = shaderMan;
+
+		setBackground(Color.white);
+		setPreferredSize(new Dimension(800, 600));
+		setLayout(null);
+
+		canvas.getCanvas().setBounds(10, 10, 500, 500);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				addCanvas();
+			}
+		});
+	}
+	
+	public void addCanvas() {
+		add(canvas.getCanvas());
+	}
+
+	public void paintComponent(Graphics _g) {
+		super.paintComponent(_g);
+		Graphics2D g = (Graphics2D) _g;
+		
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(canvas.getCanvas().getX() - 2, canvas.getCanvas().getY() - 2, canvas.getCanvas().getWidth() + 4, canvas.getCanvas().getHeight() + 4);
+	}
+}
